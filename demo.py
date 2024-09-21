66459cbdcfea685d1f9f44149115c2f5bf43db8d
ACDPDEV <acdpdev@gmail.com>
2024-09-21 09:39:28 -0500

    Build #0001
    Pequeña demo para testear 💯

---
+++ demo.py
@@ -0,0 +1,19 @@
+from src.classes.MInecraftBot import MinecraftBot
+
+ARGS = {
+    "host": "mc.servermc.xyz",
+    "port": 16126,
+    "username": "demo.py",
+    "version": "1.18.2",
+    "hideErrors": False
+}
+
+PLUGINS_ARGS = {
+    "needLogin": False,
+    "needRegister": False,
+    "password": "incorrecta"
+}
+
+RECONNECT = True
+
+bot = MinecraftBot(ARGS, PLUGINS_ARGS, RECONNECT)
